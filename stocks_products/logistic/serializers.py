45ddced2f944b23ea6a4c2cfadd1b4872e079615
Nina Spernaskaya <nina-sp@list.ru>
2022-05-07 20:32:17 +0600

    Initial commit

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    @staticmethod
+    def _update_stock_products(stock, positions):
+        for position in positions:
+            product = position.pop('product')
+            defaults = position
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=product,
+                defaults=defaults
+            )
+
+    def create(self, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # создаем склад по его параметрам
+        stock = super().create(validated_data)
+
+        # здесь вам надо заполнить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        self._update_stock_products(stock, positions)
+        return stock
+
+    def update(self, instance, validated_data):
+        # достаем связанные данные для других таблиц
+        positions = validated_data.pop('positions')
+
+        # обновляем склад по его параметрам
+        stock = super().update(instance, validated_data)
+
+        # здесь вам надо обновить связанные таблицы
+        # в нашем случае: таблицу StockProduct
+        # с помощью списка positions
+        self._update_stock_products(stock, positions)
+
+        return stock
